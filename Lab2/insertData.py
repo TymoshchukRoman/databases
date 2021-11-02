4eb4bde5066dd9bec634372331217ddaf3528a44
TymoshchukRoman <roman.tim55@gmail.com>
2021-11-02 15:19:28 +0200

    draft of insert created

--- Lab2/insertData.py
+++ Lab2/insertData.py
@@ -18,3 +18,51 @@ def insert_visitor(connection, first_name, last_name, age):
 			cursor.close()
 			connection.close()
 			print("Connection closed")
+
+
+def insert_gym(connection, address, area, fee):
+	try:
+		cursor = connection.cursor()
+		insert_query = """INSERT INTO gyms (address, area, fee)
+												VALUES (%s, %s, %s)"""
+		record_to_insert = (address, area, fee)
+		cursor.execute(insert_query, record_to_insert)
+		connection.commit()
+	except(Exception, Error) as error:
+		print("Error while working with database", error)
+	finally:
+		if connection:
+			cursor.close()
+			connection.close()
+
+
+def insert_simulator(connection, gym_id, model, weight):
+	try:
+		cursor = connection.cursor()
+		insert_query = """INSERT INTO simulators (gym_id, model, weight)
+												VALUES (%s, %s, %s)"""
+		record_to_insert = (gym_id, model, weight)
+		cursor.execute(insert_query, record_to_insert)
+		connection.commit()
+	except(Exception, Error) as error:
+		print("Error while working with database", error)
+	finally:
+		if connection:
+			cursor.close()
+			connection.close()
+
+
+def insert_abonnement(connection, visitor_id, gym_id):
+	try:
+		cursor = connection.cursor()
+		insert_query = """INSERT INTO abonnements (visitor_id, gym_id)
+												VALUES (%s, %s)"""
+		record_to_insert = (visitor_id, gym_id)
+		cursor.execute(insert_query, record_to_insert)
+		connection.commit()
+	except(Exception, Error) as error:
+		print("Error while working with database", error)
+	finally:
+		if connection:
+			cursor.close()
+			connection.close()
--- Lab2/main.py
+++ Lab2/main.py
@@ -1,7 +1,7 @@
 import psycopg2
 from config import host, user, password, db_name
 from deleteData import delete_by_id
-from insertData import insert_visitor
+from insertData import insert_abonnement, insert_gym, insert_simulator, insert_visitor
 
 try:
 	#connect to database
@@ -14,13 +14,16 @@ try:
 	connection.autocommit = True
 
 	#int objects does not support indexings
-	
-	tmp = []
-	k = input()
-	tmp.append(k)
-	delete_by_id(connection, tmp, "visitors")
+
+	# tmp = []
+	# k = input()
+	# tmp.append(k)
+	# delete_by_id(connection, tmp, "visitors")
 
 	# insert_visitor(connection, input(), input(), input())
+	# insert_gym(connection, input(), input(), input())
+	# insert_simulator(connection, input(), input(), input())
+	# insert_abonnement(connection, input(), input())
 
 except Exception as _ex:
 	print("[INFO] Error while working with PosgreSQL", _ex)
